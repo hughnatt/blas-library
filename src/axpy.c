7f1a0df98364cd04655bd15be93f684718b7c18e
Hugh Natt <hughnatt@protonmail.com>
2018-03-11 10:36:28 +0100

    Completed axpy.c and copy.c

--- src/axpy.c
+++ src/axpy.c
@@ -10,13 +10,38 @@ void cblas_saxpy(const int n, const float a, const float *x, const int incx, flo
 }
 
 void cblas_daxpy(const int n, const double a, const double *x, const int incx, double *y, const int incy){
-
+    register unsigned int i = 0 ;
+    register unsigned int j = 0 ;
+    for (; ((i < n) && (j < n)) ; i += incx, j += incy)
+    {
+      y[j] = a * x[i] + y[i] ;
+    }
 }
 
 void cblas_caxpy(const int n, const void *a, const void *x, const int incx, void *y, const int incy){
+    
+    const cplx_t** array_x = (cplx_t**) x;
+    const cplx_t** array_y = (cplx_t**) y;
+    const cplx_t* scalar_a = (cplx_t*)  a;
 
+    register unsigned int i = 0 ;
+    register unsigned int j = 0 ;
+    for (; ((i < n) && (j < n)) ; i += incx, j += incy)
+    {
+
+    }
 }
 
 void cblas_zaxpy(const int n, const void *a, const void *x, const int incx, void *y, const int incy){
+    
+    const cplxd_t** array_x = (cplxd_t**) x;
+    const cplxd_t** array_y = (cplxd_t**) y;
+    const cplxd_t* scalar_a = (cplxd_t*)  a;
+    
+    register unsigned int i = 0 ;
+    register unsigned int j = 0 ;
+    for (; ((i < n) && (j < n)) ; i += incx, j += incy)
+    {
 
+    }
 }
--- src/copy.c
+++ src/copy.c
@@ -1,42 +1,65 @@
 #include "mnblas.h"
 
-void mncblas_scopy(const int N, const float *X, const int incX, 
-                 float *Y, const int incY)
+void mncblas_scopy(const int N, const float *X, const int incX,
+                   float *Y, const int incY)
 {
-  register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
+    register unsigned int i = 0;
+    register unsigned int j = 0;
 
-  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    for (; ((i < N) && (j < N)); i += incX, j += incY)
     {
-      Y [j] = X [i] ;
+        Y[j] = X[i];
     }
 
-  return ;
+    return;
 }
 
-void mncblas_dcopy(const int N, const double *X, const int incX, 
-                 double *Y, const int incY)
+void mncblas_dcopy(const int N, const double *X, const int incX,
+                   double *Y, const int incY)
 {
-  register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
+    register unsigned int i = 0;
+    register unsigned int j = 0;
 
-  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    for (; ((i < N) && (j < N)); i += incX, j += incY)
     {
-      Y [j] = X [i] ;
+        Y[j] = X[i];
     }
 
-  return ;
+    return;
 }
 
-void mncblas_ccopy(const int N, const void *X, const int incX, 
-		           void *Y, const int incY)
+void mncblas_ccopy(const int N, const void *X, const int incX,
+                   void *Y, const int incY)
 {
+    cplx_t** array_x = (cplx_t**) X;
+    cplx_t** array_y = (cplx_t**) Y;
 
+
+    register unsigned int i = 0;
+    register unsigned int j = 0;
+    for (; ((i < N) && (j < N)); i += incX, j += incY)
+    {
+        array_y[j]->re = array_x[i]->re;
+        array_y[j]->im = array_x[i]->im;
+    }
+
+    return;
 }
 
-void mncblas_zcopy(const int N, const void *X, const int incX, 
-		           void *Y, const int incY)
+void mncblas_zcopy(const int N, const void *X, const int incX,
+                   void *Y, const int incY)
 {
+    cplxd_t** array_x = (cplxd_t**) X;
+    cplxd_t** array_y = (cplxd_t**) Y;
 
+
+    register unsigned int i = 0;
+    register unsigned int j = 0;
+    for (; ((i < N) && (j < N)); i += incX, j += incY)
+    {
+        array_y[j]->re = array_x[i]->re;
+        array_y[j]->im = array_x[i]->im;
+    }
+
+    return;
 }
-

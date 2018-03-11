61613cafdf4323239faa60ffcccff08be0ebc091
Hugh Natt <hughnatt@protonmail.com>
2018-03-11 11:56:43 +0100

    Refurbished Makfile and prepared mntests.c

--- examples/mntest.c
+++ examples/mntest.c
@@ -1,78 +1,162 @@
 #include <stdio.h>
+
+// ### Fonctions BLAS Intel
 #include <cblas.h>
 
+// ### Fonctions BLAS MN Perso
 #include "mnblas.h"
 
-/*
-  Mesure des cycles
-*/
-
+// ### Mesures des cycles
 #include <x86intrin.h>
 
+// ### Taille vecteur
+#define VECSIZE 1000
 
-#define VECSIZE    1000
+/*
+ * Type vecteur : @vfloat 
+ */
+typedef float vfloat[VECSIZE];
+vfloat vec1, vec2;
 
-typedef float vfloat [VECSIZE] ;
-
-vfloat vec1, vec2 ;
-
-void vector_init (vfloat V, float x)
+/*
+ * Initialisation d'un @vfloat avec une valeur fixe x
+ */
+void vector_init(vfloat V, float x)
 {
-  register unsigned int i ;
+    register unsigned int i;
 
-  for (i = 0; i < VECSIZE; i++)
-    V [i] = x ;
+    for (i = 0; i < VECSIZE; i++)
+        V[i] = x;
 
-  return ;
+    return;
 }
 
-void vector_print (vfloat V)
+/*
+ * Affichage d'un @vfloat en console
+ */
+void vector_print(vfloat V)
 {
-  register unsigned int i ;
+    register unsigned int i;
 
-  for (i = 0; i < VECSIZE; i++)
-    printf ("%f ", V[i]) ;
-  printf ("\n") ;
-  
-  return ;
+    for (i = 0; i < VECSIZE; i++)
+        printf("%f ", V[i]);
+    printf("\n");
+
+    return;
 }
 
-int main (int argc, char **argv)
+/*
+ * Entry Point
+ */
+int main(int argc, char **argv)
 {
- unsigned long long start, end ;
- unsigned long long residu ;
+    unsigned long long start, end;
+    unsigned long long residu;
 
- /* Calcul du residu de la mesure */
-  start = _rdtsc () ;
-  end = _rdtsc () ;
-  residu = end - start ;
-  
-  vector_init (vec1, 1.0) ;
+    // Calcul du residu de la mesure
+    start = _rdtsc();
+    end = _rdtsc();
+    residu = end - start;
 
-  start = _rdtsc () ;
-     cblas_scopy (VECSIZE, vec1, 1, vec2, 1) ;
-  end = _rdtsc () ;
+    // Initialisation vec1, valeur : 1.0
+    vector_init(vec1, 1.0);
 
-  printf ("cblas_scopy1 nombre de cycles cblas: %Ld \n", end-start-residu) ;
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES COPY @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+    
+    // --- scopy ---
+    start = _rdtsc();
+    cblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+    end = _rdtsc();
+    printf("cblas_scopy1 nombre de cycles cblas: %Ld \n", end - start - residu);
+    
+    start = _rdtsc();
+    mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+    end = _rdtsc();
+    printf("mncblas_scopy: nombre de cycles: %Ld \n", end - start - residu);
+    /* 
+    printf ("Vector 2:\n") ;
+    vector_print (vec2) ;
+    */
+
+    start = _rdtsc();
+    cblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+    end = _rdtsc();
+    printf("cblas_scopy2 nombre de cycles cblas: %Ld \n", end - start - residu);
 
 
-  
-  start = _rdtsc () ;
-     mncblas_scopy (VECSIZE, vec1, 1, vec2, 1) ;
-  end = _rdtsc () ;
+    // @@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINE ASUM @@@
+    // @@@@@@@@@@@@@@@@@@@@
 
-  /*
-  printf ("Vector 2:\n") ;
-  vector_print (vec2) ;
-  */
-  
-  printf ("mncblas_scopy: nombre de cycles: %Ld \n", end-start-residu) ;
+    // --- sasum ---
+    // --- scasum ---
+    // --- dasum ---
+    // --- dzasum ---
 
-  start = _rdtsc () ;
-     cblas_scopy (VECSIZE, vec1, 1, vec2, 1) ;
-  end = _rdtsc () ;
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES AXPY @@@
+    // @@@@@@@@@@@@@@@@@@@@@
 
-  printf ("cblas_scopy2 nombre de cycles cblas: %Ld \n", end-start-residu) ;
+    // --- saxpy
+    // --- daxpy
+    // --- caxpy
+    // --- zaxpy
 
-  
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES COPY @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // --- scopy
+    // --- dcopy
+    // --- ccopy
+    // --- zcopy
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES DOT  @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // --- sdot
+    // --- ddot
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES DOTC @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+    
+    // --- zdotc_sub
+    // --- cdotc_sub
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES DOTU @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // --- zdotu_sub
+    // --- cdotu_sub
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES GEMM @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES GEMV @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES NRM2 @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // --- snrm2
+    // --- dnrm2
+    // --- scnrm2
+    // --- dznrm2
+
+    // @@@@@@@@@@@@@@@@@@@@@
+    // @@@ ROUTINES SWAP @@@
+    // @@@@@@@@@@@@@@@@@@@@@
+
+    // --- sswap
+    // --- dswap
+    // --- cswap
+    // --- zswap
 }
--- src/gemm.c
+++ src/gemm.c
@@ -4,22 +4,26 @@ void cblas_sgemm(const MNCBLAS_LAYOUT Layout, const MNCBLAS_TRANSPOSE transa, co
                  const int m, const int n, const int k, const float alpha, const float *a, const int lda,
                  const float *b, const int ldb, const float beta, float *c, const int ldc)
 {
+    /* a completer */
 }
 
 void cblas_dgemm(const MNCBLAS_LAYOUT Layout, const MNCBLAS_TRANSPOSE transa, const MNCBLAS_TRANSPOSE transb,
                  const int m, const int n, const int k, const double alpha, const double *a, const int lda,
                  const double *b, const int ldb, const double beta, double *c, const int ldc)
 {
+    /* a completer */
 }
 
 void cblas_cgemm(const MNCBLAS_LAYOUT Layout, const MNCBLAS_TRANSPOSE transa, const MNCBLAS_TRANSPOSE transb,
                  const int m, const int n, const int k, const void *alpha, const void *a, const int lda,
                  const void *b, const int ldb, const void *beta, void *c, const int ldc)
 {
+    /* a completer */
 }
 
 void cblas_zgemm(const MNCBLAS_LAYOUT Layout, const MNCBLAS_TRANSPOSE transa, const MNCBLAS_TRANSPOSE transb,
                  const int m, const int n, const int k, const void *alpha, const void *a, const int lda,
                  const void *b, const int ldb, const void *beta, void *c, const int ldc)
 {
+    /* a completer */
 }
